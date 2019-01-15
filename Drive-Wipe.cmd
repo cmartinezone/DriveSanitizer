4dcb2976bf328e3262e9fd23c4b85a8a22a7be35
Carlos Martinez <carlos0492@outlook.com>
2019-01-15 13:44:30 -0500

    Update Drive-Wipe.cmd

--- Drive-Wipe.cmd
+++ Drive-Wipe.cmd
@@ -14,26 +14,8 @@ cls
 
 ::Call SDelete to wipe the drive in 3 passes, free space, zero space
 SDelete\sdelete64.exe -p 3 -c -z %Dn% 
-
-::Change the Dn varialble value for ghost64 which it works with +1 drive index 
-set /A Dn+=1
-
-::Call ghost to apply the MSDOS image to the drive cleaned 
-MSDOSGhost\ghost64.exe -batch -clone,mode=restore,src=MSDOSGhost\dos.gho,dst=%Dn% -sure -quiet -blind -pmbr
-
-If %ERRORLEVEL% NEQ 0 goto Error
-
-::Restore variable initial value 
-set /A Dn-=1
 title Disk %Dn% Completed!
 echo Drive has been successfully wiped.
-goto Edn
-
-::Error function if Ghost output error level change
-:Error
-set /A Dn-=1
-title Disk %Dn% Error!
-echo Something went wrong, please try again!
 goto Edn
 
 ::It Keeps the console open
